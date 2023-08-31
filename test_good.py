de0adb164d30e74cd8876bf0ab9f9794582d9a84
Daria <dariamoscow98@gmail.com>
2023-08-31 21:41:29 +0600

    added new expect by ad url

--- test_good.py
+++ test_good.py
@@ -2,16 +2,18 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
+    ad_url = "https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363"
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363")
-    favourite = page.locator("button").filter(has_text="Добавить в избранное")
-    favourite.click()
+    page.goto(ad_url)
+    ad_title = page.locator(".title-info-title-text").inner_text()
+    page.locator("button").filter(has_text="Добавить в избранное").click()
     page.get_by_role("link", name="Избранное", exact=True).click()
-    name_text = page.locator("strong").filter(has_text="Domain-Driven Design Distilled Vaughn Vernon")
-    expect(name_text).to_contain_text("Domain-Driven Design Distilled Vaughn Vernon")
-    expect(favourite.page).to_have_url('https://www.avito.ru/favorites')
+    ad_locator = page.locator("strong").filter(has_text=ad_title)
+    expect(ad_locator).to_contain_text(ad_title)
+    ad_locator.click()
+    expect(page).to_have_url(ad_url)
 
     # -----------------------
     context.close()
