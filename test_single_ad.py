57220c426f11629d5cb4f4d00e3e7667fe4594cd
Daria <dariamoscow98@gmail.com>
2023-08-31 22:09:31 +0600

    added pytest runner

--- test_single_ad.py
+++ test_single_ad.py
@@ -1,11 +1,8 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Page, expect
 
 
-def run(playwright: Playwright) -> None:
+def test_add_ad_to_favourites(page: Page):
     ad_url = "https://www.avito.ru/nikel/knigi_i_zhurnaly/domain-driven_design_distilled_vaughn_vernon_2639542363"
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
     page.goto(ad_url)
     ad_title = page.locator(".title-info-title-text").inner_text()
     page.locator("button").filter(has_text="Добавить в избранное").click()
@@ -14,11 +11,3 @@ def run(playwright: Playwright) -> None:
     expect(ad_locator).to_contain_text(ad_title)
     ad_locator.click()
     expect(page).to_have_url(ad_url)
-
-    # -----------------------
-    context.close()
-    browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
